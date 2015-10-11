24c5fb7538f9522c7cc8d6632624e6e3316a5f2f
yanai <yanai@tikalk.com>
2015-10-11 20:42:02 +0300

    Vertx 3.1

--- src/main/java/com/tikal/heatmap/ReactorVerticle.java
+++ src/main/java/com/tikal/heatmap/ReactorVerticle.java
@@ -1,21 +1,28 @@
 package com.tikal.heatmap;
 
+import com.cyngn.kafka.MessageProducer;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.buffer.Buffer;
 
 public class ReactorVerticle extends AbstractVerticle {
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ReactorVerticle.class);
 
 	@Override
 	public void start(){
-		vertx.deployVerticle("com.tikal.heatmap.KafkaProducerVerticle",new DeploymentOptions().setConfig(config()));
+		vertx.deployVerticle(MessageProducer.class.getName(),new DeploymentOptions().setConfig(config()));		
 		
 		vertx.createHttpServer()
-			.requestHandler(r -> r.bodyHandler(b ->	vertx.eventBus().send(config().getString("address"), b.toString()))
+			.requestHandler(r -> r.bodyHandler(b ->	sendToKafka(b))
 			.response().end())
 			.listen(config().getInteger("http.server.port"));
 		
 		logger.info("HTTP server is listening on port {} ...",config().getInteger("http.server.port"));
 	}
 
+	private void sendToKafka(final Buffer b) {
+		logger.debug("Sending to Kafka:{}",b.toString());
+		vertx.eventBus().send(MessageProducer.EVENTBUS_DEFAULT_ADDRESS, b.toString());
+	}
 }
